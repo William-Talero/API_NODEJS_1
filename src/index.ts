6fb35a6d78d47a023025c36238fcefe70d0a9b87
williamTalero04 <wtalero13983@universidadean.edu.co>
2023-01-15 11:01:33 -0500

    Was add the strictQuery option to moongose

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ app.get('/', (req, res) => {
 
 // Connect to MongoDB
 mongoose
+  .set('strictQuery', false)
   .connect(process.env.MONGODB_URI!)
   .then(() => {
     console.log('Connected to MongoDB Atlas.');
