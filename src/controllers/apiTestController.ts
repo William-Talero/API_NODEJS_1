33a38d9457bf93a06642684f457ee961fcfcc0f7
williamTalero04 <wtalero13983@universidadean.edu.co>
2023-01-15 20:32:36 -0500

    Was change the name response fot data in the API answer

--- src/controllers/apiTestController.ts
+++ src/controllers/apiTestController.ts
@@ -2,30 +2,30 @@ import ApiTestService from '../services/apiTestService';
 
 const createApiTest = async(req, res) => {
   const newApiTest = await ApiTestService.createApiTest(req.body);
-  res.send({ status: 200, response: newApiTest });
+  res.send({ status: 200, data: newApiTest });
 };
 
 const getAllApiTest = async (req, res) => {
   const allApiTest = await ApiTestService.getAllApiTest();
-    res.send({ status: 200, response: allApiTest });
+    res.send({ status: 200, data: allApiTest });
 };
 
 const getSpecificApiTest = async(req, res) => {
   const { id } = req.params;
   const specificApiTest = await ApiTestService.getSpecificApiTest(id);
-  res.send({ status: 200, response: specificApiTest });
+  res.send({ status: 200, data: specificApiTest });
 };
 
 const updateSpecificApiTest = async(req, res) => {
   const { id } = req.params;
   const updatedApiTest = await ApiTestService.updateSpecificApiTest(id, req.body);
-  res.send({ status: 200, response: updatedApiTest });
+  res.send({ status: 200, data: updatedApiTest });
 };
 
 const deleteSpecificApiTest = async(req, res) => {
   const { id } = req.params;
   const deletedApiTest = await ApiTestService.deleteSpecificApiTest(id);
-  res.send({ status: 200, response: deletedApiTest });
+  res.send({ status: 200, data: deletedApiTest });
 };
 
 export default {
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,31 +2,31 @@ import userService from './../services/userService';
 
 const createUser = async(req, res) => {
   const newUser = await userService.createUser(req.body);
-  res.send({ status: 200, response: newUser });
+  res.send({ status: 200, data: newUser });
 };
 
 const getAllUsers = async (req, res) => {
   const allUsers = await userService.getAllUsers();
-    res.send({ status: 200, response: allUsers });
+    res.send({ status: 200, data: allUsers });
 };
 
 
 const getSpecificUser = async(req, res) => {
   const { id } = req.params;
   const specificUser = await userService.getSpecificUser(id);
-  res.send({ status: 200, response: specificUser });
+  res.send({ status: 200, data: specificUser });
 };
 
 const updateSpecificUser = async(req, res) => {
   const { id } = req.params;
   const updatedUser = await userService.updateSpecificUser(id, req.body);
-  res.send({ status: 200, response: updatedUser });
+  res.send({ status: 200, data: updatedUser });
 };
 
 const deleteSpecificUser = async(req, res) => {
   const { id } = req.params;
   const deletedUser = await userService.deleteSpecificUser(id);
-  res.send({ status: 200, response: deletedUser });
+  res.send({ status: 200, data: deletedUser });
 };
 
 export default {
